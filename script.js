acad9182defbd93fccff042c8bf868cda5b908d9
Kaustav Pratick Hazarika <hazarikakaustav23@gmail.com>
2024-03-31 22:27:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Navbar togglemenu start
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+// Navbar togglemenu end
+
